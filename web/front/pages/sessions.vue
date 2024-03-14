b6dd7da9f0b2da7c059f32cb41fd4607c7c24eee
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2024-03-14 09:29:36 +0100

    demanar peli function created

--- web/front/pages/sessions.vue
+++ web/front/pages/sessions.vue
@@ -1,6 +1,9 @@
 <template>
     <section>
       <p>This page will be displayed at the /sessions route.</p>
+      <div v-for="item in sessions.records">
+        <h1>Sessió {{item.id}} amb peli {{demanarPeli(item.id_peli).titol}}</h1>
+      </div>
     </section>
 </template>
 
@@ -8,7 +11,16 @@
   const {pending, data: sessions}=useLazyFetch('http://tr3marbenalc.daw.inspedralbes.cat/back/api.php/records/Sessio',{
     method:'GET',
     onResponse(){
-      console.log(sessions);
+      console.log(sessions.records);
     }
   });
+  function demanarPeli(id_peli) {
+    const {pending, data: peli}=useLazyFetch(`http://tr3marbenalc.daw.inspedralbes.cat/back/api.php/records/Pelicula/${id_peli}`,{
+      method:'GET',
+      onResponse(){
+        console.log(peli);
+        return peli;
+      }
+    });
+  }
 </script>
