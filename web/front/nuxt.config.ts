c047dec4c35a4fce35d913d8556244efa185ba0c
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2024-03-15 10:10:33 +0100

    ssr simple a nuxt.config.ts

--- web/front/nuxt.config.ts
+++ web/front/nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  routeRules: {
+    // Homepage pre-rendered at build time
+    '/': { ssr: true },
+    '/**':{ssr:false}
+    /*
+    // Products page generated on demand, revalidates in background, cached until API response changes
+    'products': { swr: true },
+    // Product page generated on demand, revalidates in background, cached for 1 hour (3600 seconds)
+    '/products/**': { swr: 3600 },
+    // Blog posts page generated on demand, revalidates in background, cached on CDN for 1 hour (3600 seconds)
+    '/blog': { isr: 3600 },
+    // Blog post page generated on demand once until next deployment, cached on CDN
+    '/blog/**': { isr: true },
+    // Admin dashboard renders only on client-side
+    '/admin/**': { ssr: false },
+    // Add cors headers on API routes
+    '/api/**': { cors: true },
+    // Redirects legacy urls
+    '/old-page': { redirect: '/new-page' }*/
+  }
 })
