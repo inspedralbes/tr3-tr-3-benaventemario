9ea738c2619a7ab55bb3842ba9b8bc264bd42430
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2024-04-09 13:12:45 +0200

    Login modal en procés

--- web/front/app.vue
+++ web/front/app.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="cine">
-    <h1>APP</h1>
     <header>
-      <nav>
-        <ul>
-          <span>
-            <NuxtLink to="/">Home</NuxtLink>
-            <NuxtLink v-if="(storeUsuari.comprovarAdmin)==true" to="/admin">Admin</NuxtLink>
-          </span>
-        </ul>
-      </nav>
+      <h2>CINEMAR</h2>
+        <nav class="menu">
+            <span class="menu__esquerra">
+                <NuxtLink to="/">Home</NuxtLink>
+            </span>
+            <span class=menu__dreta>
+                <NuxtLink v-if="(storeUsuari.comprovarAdmin)==true" to="/admin">Admin</NuxtLink>
+                <NuxtLink @click="mostrarLogin=true">Login</NuxtLink>
+                <span v-if="storeUsuari.mostrarCorreu!=''"></span>
+            </span>
+        </nav>
     </header>
+    <Teleport to="body">
+      <loginPopup v-if="mostrarLogin==true" :mostrarLogin="mostrarLogin"/>
+    </Teleport>
     <NuxtPage />
 
   </div>
 </template>
 
 <script setup>
-import { useUsuariStore } from "~/stores/usuari";
+    import { useUsuariStore } from "~/stores/usuari";
     const storeUsuari=useUsuariStore();
+    const mostrarLogin=ref(false);
 </script>
---
+++ web/front/components/loginPopup.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+<!-- https://dev.to/davidemaye/how-to-create-a-modal-component-in-nuxt-38g1
+https://stackoverflow.com/questions/75440449/nuxt3-vue3-how-to-show-modal-component-that-is-called-from-header-component -->
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
