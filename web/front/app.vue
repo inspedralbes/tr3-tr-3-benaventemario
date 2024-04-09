f2112ba471eaa486a08ef844ab8848c03619aec0
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2024-04-09 10:04:33 +0200

    popups endesenvolupament

--- web/front/app.vue
+++ web/front/app.vue
@@ -1,15 +1,18 @@
 <template>
-  <h1>APP</h1>
-  <header>
-    <nav>
-      <ul>
-        <span>
-          <NuxtLink to="/">Home</NuxtLink>
-        </span>
-      </ul>
-    </nav>
-  </header>
-  <NuxtPage />
+  <div class="cine">
+    <h1>APP</h1>
+    <header>
+      <nav>
+        <ul>
+          <span>
+            <NuxtLink to="/">Home</NuxtLink>
+          </span>
+        </ul>
+      </nav>
+    </header>
+    <NuxtPage />
+
+  </div>
 </template>
 
 <script setup>
--- web/front/components/FitxaSessio.vue
+++ web/front/components/FitxaSessio.vue
@@ -1,34 +1,54 @@
 <template>
-        <img  @click="mostrarDetalls=true" v-if="!mostrarDetalls" width="150" height="220" :src="sessio.peli.imatge" :alt="`poster de ${sessio.peli.titol}`" srcset="">
+        <img  @click="obrirOTancarPopup(true)" width="150" height="220" :src="sessio.peli.imatge" :alt="`poster de ${sessio.peli.titol}`" srcset="">
         <!-- <UModal v-model="isOpen">
             <div class="p-4">
                 <Placeholder class="h-48" />
             </div>
         </UModal> -->
-        <div class="sessióPopup__marc" v-if="mostrarDetalls">
-            <div class="sessioPopup__interior">
+        <Teleport to="body">
+            <div class="sessioPopup" v-if="mostrarDetalls">
                 <h2>{{ sessio.peli.titol }}</h2>
-                <img width="300" height="440" :src="sessio.peli.imatge" :alt="`poster de ${sessio.peli.titol}`" srcset="">
-                <h2>{{ sessio.peli.descripcio }}</h2>
-                <button @click="mostrarDetalls=false" aria-label="Tancar Popup" class="fitxaSessió__popup_botoTancament">X</button>
-                <button @click="recarregarSessio=true"><NuxtLink to="/sala">Reservar entrades</NuxtLink></button>
+                <img width="250" height="366" :src="sessio.peli.imatge" :alt="`poster de ${sessio.peli.titol}`" srcset="">
+                <span>{{ sessio.peli.descripcio }}</span>
+                <button @click="obrirOTancarPopup(false)" aria-label="Tancar Popup" class="sessioPopup__botoTancament">&times;</button>
+                <button><NuxtLink to="/sala">Reservar entrades</NuxtLink></button>
             </div>
-        </div>
+        </Teleport>
 </template>
 
 <script setup>
     import { useSessioStore } from "~/stores/sessio";
     const storeSessio=useSessioStore();
-
+    
+    import { ref } from 'vue';
     const mostrarDetalls = ref(false)
     const {sessio}=defineProps(['sessio'])
 
     function recarregarSessio() {
         storeSessio.novaSessio(sessio)
     }
+
+    function obrirOTancarPopup(nouValor) {
+        mostrarDetalls.value=nouValor;
+        if (nouValor==true) {
+            recarregarSessio();
+            document.body.className="sessioPopup_actiu"
+        }else{
+            document.body.className="buit"
+        }
+        // useHead({
+        //     bodyAttrs: {
+        //         class: computed(() => {
+        //             if (mostrarDetalls===true){
+        //                 return 'sessioPopup_actiu';
+        //             }else{
+        //                 return 'buit';
+        //             }
+        //         }),
+        //     },
+        // });
+    }
 </script>
 
-<style scoped>
-
-
+<style>
 </style>
