0f429d2e1e0695eebd017f14b9c9a9acc0385a5e
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2024-04-19 13:00:10 +0200

    cypress, test#2

--- web/front/cypress/e2e/spec.cy.js
+++ web/front/cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe("Test d'inici de sessió", () => {
+  it('Inicia sessió al transversal 3 i llegeix que el username és el correcte', () => {
+    cy.visit('https://tr3marbenalc.daw.inspedralbes.cat/')
+    cy.get('.menu__dreta_registre').click()
+    cy.get('.modal__campCorreu').type('fake@email.com')
+    cy.get('.modal__enregistrament').click()
+    cy.get('.menu__dreta').contains('fake')
   })
 })
