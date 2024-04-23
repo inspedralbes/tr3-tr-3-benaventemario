0d64d8386e7bd88f41474a34b2b2e890345a5f27
MarioBenPedralbes <a21marbenalc@inspedralbes.cat>
2024-04-23 13:17:23 +0200

    continuació cypress

--- web/front/cypress/e2e/crudUse.cy.js
+++ web/front/cypress/e2e/crudUse.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("Test d'administració", () => {
+  it('Inicia sessió al transversal 3 i llegeix que el username és el correcte', () => {
+    cy.visit('https://tr3marbenalc.daw.inspedralbes.cat/')
+    cy.get('.menu__dreta_registre').click()
+    cy.get('.modal__campCorreu').type('a21marbenalc@inspedralbes.cat')
+    cy.get('.modal__enregistrament').click()
+    cy.contains('Admin').click()
+    cy.url().should('include', '/admin')
+    cy.get('.admin__control').contains('Crear').click()
+    cy.get('#peli').select(3)
+    cy.get('#dia').type('2024-05-02')
+    cy.get('#hora').select(2)
+    cy.get('#vip').select(1)
+    cy.get('#descompte').select(1)
+    cy.get(".adminTaula table")
+    .find("tr")
+    .then((row) => {
+      cy.log(row.length);
+    });
   })
 })
---
+++ web/front/cypress/e2e/purchaseFlux.cy.js
@@ -0,0 +1,5 @@
+describe("Test d'administració", () => {
+  it('Inicia sessió al transversal 3 i llegeix que el username és el correcte', () => {
+    cy.visit('https://tr3marbenalc.daw.inspedralbes.cat/')
+  })
+})
